4dea02444e49e4884b78caf8845aab0b562e6427
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-08-15 18:07:33 +0530

    v

--- frontend/src/components/Model.jsx
+++ frontend/src/components/Model.jsx
@@ -5,7 +5,7 @@ import { GoArrowUpRight } from "react-icons/go";
 export default function Model() {
   return (
     <div className="bg-customBlack2 flex m-auto w-[90%] sm:w-[85%] md:w-[80%] lg:w-[65%] rounded-lg h-60 sm:h-80">
-      <div className="w-5/6 sm:w-4/5 text-center mx-auto space-y-7 py-14 sm:py-24">
+      <div className="w-5/6 sm:w-4/5 text-center mx-auto space-y-4 sm:space-y-7 py-8 sm:py-24">
         <div>
           <HighLightText2
             text="Master your data, seize your life, shape your future with OvaDrive"
